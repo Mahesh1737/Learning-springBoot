dc7612609987c548a34b21e52a9a594930ce5bf7
Mahesh1737 <maheshmore1737@gmail.com>
2025-09-12 22:13:36 +0530

    In spring security configuration/authorization/authentication used

--- Spring-boot/Spring-Security/spring-sec-p02/spring-sec-p02/src/main/java/com/mrm/spring_sec_p02/Controller/BankController.java
+++ Spring-boot/Spring-Security/spring-sec-p02/spring-sec-p02/src/main/java/com/mrm/spring_sec_p02/Controller/BankController.java
@@ -1,4 +1,37 @@
 package com.mrm.spring_sec_p02.Controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
 public class BankController {
+
+        int balance = 3000;
+    @GetMapping("/bal")
+    public int getbal(){
+        return balance;
+    }
+
+    @GetMapping("/contact")
+    public String ContactUs(){
+        return "Contact us at: 8092489987";
+    }
+
+    @GetMapping("/about")
+    public String aboutUs(){
+        return "Welcome to about us page";
+    }
+
+    @GetMapping("/admin")
+    public String adminLogin(){
+        return "Welcome admiin";
+    }
+
+    @GetMapping("/transfer")
+    public String TransferMoney(){
+        return "Money tranfer successfully!";
+    }
+
 }
